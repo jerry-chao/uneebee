c6ceff9139482d73769e80cfc9f43f6445b187fb
jerry.zhang <jerry.zhang>
2023-11-23 11:49:38 +0800

    remove https for local dev

--- config/dev.exs
+++ config/dev.exs
@@ -20,13 +20,6 @@ config :uneebee, UneebeeWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   http: [ip: {0, 0, 0, 0}, port: 4000],
-  https: [
-    ip: {0, 0, 0, 0},
-    port: 4001,
-    cipher_suite: :strong,
-    keyfile: "priv/cert/selfsigned_key.pem",
-    certfile: "priv/cert/selfsigned.pem"
-  ],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
